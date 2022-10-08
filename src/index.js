3179cc72be0ca33a26ebb7af31ff85b749db7bbd
pdcourses <freshitpt@gmail.com>
2022-10-08 11:39:08 +0300

    test table

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const { User } = require("./db/models");
+
+/*
+User.create({
+    name: 'Test2',
+    surname: 'Test2',
+    email: 'test2@mail.com'
+}).then(console.log);
+
+User.findByPk(2).then(console.log);
+*/
+
+User.findAll({ attributes: { exclude: ["createdAt", "updatedAt"] } }).then(console.log);
