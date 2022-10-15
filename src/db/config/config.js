87d5e239910bb6a61cbed23b3f585c3feafed8c5
pdcourses <freshitpt@gmail.com>
2022-10-15 14:00:17 +0300

    sequelizerc and dotenv were added

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,9 +1,9 @@
-//require('dotenv/config');
+require('dotenv/config');
 module.exports = {
   development: {
-    username: process.env.DB_USER || "julia",
-    password: process.env.DB_PASSWORD || "admin",
-    database: process.env.DB_NAME || "todo_pd2022",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT || 5432,
     dialect: "postgres",
